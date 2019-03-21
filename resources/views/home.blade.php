e8e382fc2fbede1fdb66953c0fbfd0eaef6837e4
Guilherme Santos <guilhermedev@hotmail.com>
2019-03-21 14:28:57 -0300

    first commit

---
+++ resources/views/home.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.master')
+@section('title', 'Dasboard')
+@section('content')
+<div class="container">
+        <div class="col-md-12">
+            <div class="card card-default">
+                <div class="card-header">Dashboard</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    You are logged in!
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
